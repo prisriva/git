91dcb2c7460da53b473523401fb0b30baac8dc26
prisriva <prisriva004@gmail.com>
2025-03-23 13:52:53 -0700

    new

--- assignment_6/script.js
+++ assignment_6/script.js
@@ -1,12 +1,12 @@
 // Display a message in the browser console
 console.log("CSS + JavaScript is powerful!");
 
-// Show an alert message when the page loads
+// popup allert message
 window.onload = function () {
     alert("Welcome to Assignment 5 with JavaScript!");
 };
 
-// Function to toggle between light mode and dark mode
+// light and dark mode
 function changeTheme() {
     document.body.classList.toggle("dark-mode");
 }
